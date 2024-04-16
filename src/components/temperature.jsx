3ff2fa54effb696e14378cde1bb571adeb3ad1f9
Binitstha <binitcr336@gmail.com>
2024-04-16 23:39:38 +0545

    adding Day status with icons

--- src/components/temperature.jsx
+++ src/components/temperature.jsx
@@ -1,11 +1,16 @@
 import { PropTypes } from "prop-types";
 import { useEffect, useState } from "react";
 import Loading from "./animation/loading";
+import icons from "../icons/weatherIcon";
+
 export const Temperature = ({ weatherData }) => {
   const [temperature, setTemperature] = useState("");
   const [feelsLiketemperature, setFeelsLikeTemperature] = useState("");
   const [sunrise, setSunrise] = useState("");
   const [sunset, setSunset] = useState("");
+  const [conditions, setConditions] = useState("");
+  const [conditionsIcon, setConditionsIcon] = useState("");
+
   useEffect(() => {
     if (!(!weatherData || !weatherData.currentConditions)) {
       setTemperature(`${Math.floor(weatherData.currentConditions.temp)}°F`);
@@ -14,6 +19,8 @@ export const Temperature = ({ weatherData }) => {
       );
       setSunrise(weatherData.currentConditions.sunrise);
       setSunset(weatherData.currentConditions.sunset);
+      setConditions(weatherData.currentConditions.conditions);
+      setConditionsIcon(weatherData.currentConditions.icon);
     }
   }, [weatherData]);
 
@@ -29,6 +36,11 @@ export const Temperature = ({ weatherData }) => {
     return formattedTime;
   };
 
+  console.log(conditionsIcon);
+
+  const weatherIcon = icons
+    .map((icon) => icon[conditionsIcon]).filter(icon=>icon)[0]
+  console.log("icon", weatherIcon);
   return (
     <>
       <div className=" flex gap-5 justify-start items-center h-64 w-full rounded-xl bg-slate-600 p-3 ">
@@ -48,7 +60,7 @@ export const Temperature = ({ weatherData }) => {
           </div>
           <div className="h-36 flex flex-col justify-evenly gap-2 items-center p-1">
             <div className="flex gap-3 text-xl">
-              <i className="flex items-center justify-center fa-light fa-sunrise fa-fade text-4xl"></i>
+              <i className="flex items-center justify-center fa-light fa-sunrise text-4xl"></i>
               <div className="flex flex-col justify-center items-start">
                 <span>Sunrise</span>
                 <span className="text-base">{convertTimeFormat(sunrise)}</span>
@@ -56,26 +68,26 @@ export const Temperature = ({ weatherData }) => {
             </div>
             <div>
               <div className="flex gap-3 text-xl">
-                <i className="flex justify-center items-center fa-light fa-sunset fa-fade text-4xl"></i>
+                <i className="flex justify-center items-center fa-light fa-sunset text-4xl"></i>
                 <div className="flex flex-col justify-center items-start">
                   <span>Sunset</span>
-                  <span className="text-base">
-                    {convertTimeFormat(sunset)}
-                  </span>
+                  <span className="text-base">{convertTimeFormat(sunset)}</span>
                 </div>
               </div>
             </div>
           </div>
         </section>
-        <section className="flex flex-col">
-          <div>Img</div>
-          <div>day status</div>
+        <section className="flex flex-col h-full w-48 justify-evenly items-center gap-3">
+          <div className="h-32 w-32 flex justify-center items-center">
+          <i className={`${weatherIcon} text-9xl`}></i>
+          </div>
+          <div className="text-xl">{conditions}</div>
         </section>
-        <section className="flex flex-wrap">
-          <div>humidity</div>
-          <div>wind speed</div>
-          <div>Pressure</div>
-          <div>UV</div>
+        <section className="flex flex-wrap border-2 w-64 h-full justify-center items-center gap-2">
+          <div className="h-28 flex justify-center items-center border-2 w-28">humidity</div>
+          <div className="h-28 flex justify-center items-center border-2 w-28">wind speed</div>
+          <div className="h-28 flex justify-center items-center border-2 w-28">Pressure</div>
+          <div className="h-28 flex justify-center items-center border-2 w-28">UV</div>
         </section>
       </div>
     </>
---
+++ src/icons/weatherIcon.js
@@ -0,0 +1,22 @@
+const icons = [
+  {
+    "snow": "fa-duotone fa-snowflake fa-bounce",
+    "snow-showers-day": "fa-duotone fa-snowflake-droplets fa-beat-fade",
+    "snow-showers-night": "fa-duotone fa-snowflake-droplets fa-beat-fade",
+    "thunder-rain": "fa-sharp fa-duotone fa-cloud-bolt fa-shake",
+    "thunder-showers-day": "fa-duotone fa-cloud-bolt-sun",
+    "thunder-showers-night": "fa-duotone fa-cloud-bolt-moon",
+    "rain": "fa-duotone fa-cloud-rain fa-beat-fade",
+    "showers-day": "fa-duotone fa-cloud-showers-water fa-bounce",
+    "showers-night": "fa-duotone fa-cloud-showers-water fa-bounce",
+    "fog": "fa-duotone fa-smoke fa-fade",
+    "wind": "fa-duotone fa-wind fa-fade",
+    "cloudy": "fa-duotone fa-clouds",
+    "partly-cloudy-day": "fa-duotone fa-clouds-sun fa-fade",
+    "partly-cloudy-night": "fa-duotone fa-clouds-moon fa-fade",
+    "clear-day": "fa-duotone fa-sun fa-fade",
+    "clear-night": "fa-duotone fa-moon-stars fa-fade",
+  },
+];
+
+export default icons
